eb61380b6292a9c6b8d3de003ff09cc7c0c1366f
Mateusz <wszeborowskimateusz@gmail.com>
2018-05-18 19:03:16 +0200

    Now program takes parameters

--- MN_Projekt3/main.cpp
+++ MN_Projekt3/main.cpp
@@ -9,14 +9,25 @@
 
 int main(int argc, char *argv[]) {
 
-	Points p(File::extractCoordinatesFromFile("stale.txt"));
-
-	p = Interpolation::Spline(p, 10, 10);
-
-	File::saveInterpolationResultsToFile(p, "result");
-	p.print();
-	
-	
+	/*
+	argv[1] - file name
+	argv[2] - how many points to use from the file
+	argv[3] - delta x
+	argv[4] - S for Spline and L for Lagrange
+	argv[5] - result file name - without extension
+	*/
+	if (argc > 5) {
+		
+		Points p(File::extractCoordinatesFromFile(argv[1]));
+		if (!strcmp(argv[4],"S") || !strcmp(argv[4],"L")) {
+			if (!strcmp(argv[4],"S"))
+				p = Interpolation::Spline(p, std::stoi(argv[2]), std::stoi(argv[3]));
+			else if (!strcmp(argv[4],"L"))
+				p = Interpolation::Lagrange(p, std::stoi(argv[2]), std::stoi(argv[3]));
+			File::saveInterpolationResultsToFile(p, argv[5]);
+			p.print();
+		}
+	}
 
 	return 0;
 }
